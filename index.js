2a407c5ac5a019306973a1548c7cb277c29daa9b
vijaypandian <ananth.vijay@gmail.com>
2021-02-18 16:26:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 const e = React.createElement;
-
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { clicked: false };
+  }
 const domContainer = document.querySelector('#button_container');
 ReactDOM.render(e(Button), domContainer);
