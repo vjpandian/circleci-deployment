8cff8d3fa214c5ad5da8ef20507e254ab5b3f589
Vijay Pandian <vijay.pandian@invisor.ca>
2021-02-18 16:40:11 -0500

    Adding button element to TestButton.js

--- src/components/TestButton.js
+++ src/components/TestButton.js
@@ -1,4 +1,13 @@
 import React from 'react'
+
 const TestButton = () => {
-    const [counter, setCounter] = React.useState(0)
-export default TestButton
+ const [counter, setCounter] = React.useState(0)
+  
+ return (
+  <>
+    <button data-testid="button_container" onClick={() => setCounter(counter + 1)}> Up</button>
+ </>
+    )
+  }
+  
+  export default TestButton
